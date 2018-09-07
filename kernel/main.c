961630d6ed7546089adae9d5f101215c834ac3ed
Chosen_debugger <1023578360@qq.com>
2018-09-08 01:34:39 +0800

    Vol.0.2

--- kernel/main.c
+++ kernel/main.c
@@ -114,3 +114,146 @@ void TestA()
 {
 
 }
+
+
+#define MAX_FILE_NAME_LEN 30
+
+#define SYS_DATA_FILE_NAME "dataFile"
+
+
+//文件ID  文件名  文件类型（-1为空、0为文件夹、1为文件）  父节点ID  
+//0 孩子数  孩子ID数组
+//1 文本字符串
+struct FCB {
+
+
+
+};
+
+
+//从虚拟硬盘中加载初始文件系统
+void FMInit()
+{
+
+}
+
+void ReadCommand()
+{
+
+}
+
+//创建空文件
+void Command_touch()
+{
+
+}
+
+//创建空文件夹
+void Command_mkdir()
+{
+
+}
+
+//删除文件
+void Command_rm()
+{
+
+}
+
+//删除空文件夹
+void Command_rmdir()
+{
+
+}
+
+//查看文件
+void Command_file()
+{
+
+}
+
+//查看文件夹
+void Command_ls()
+{
+
+}
+
+//改变路径
+void Command_cd()
+{
+
+}
+
+//保存当前文件系统
+void Command_sv()
+{
+
+}
+
+//帮助菜单
+void ShowHelp()
+{
+
+}
+
+//输入指令有误
+void CommandWrong()
+{
+
+}
+
+//文件系统
+//要实现的指令：
+//touch 创建空文件
+//mkdir 创建空文件夹
+//rm	删除文件
+//rmdir	删除空文件夹
+//file	查看文件
+//ls	查看当前路径下文件列表
+//cd	更改路径
+//sv	保存当前文件系统
+
+char currentCmd[128];
+
+void TestB()
+{
+	int fd_stdin = open("/dev_tty1", O_RDWR);
+	assert(fd_stdin == 0);
+	int fd_stdout = open("/dev_tty1", O_RDWR);
+	assert(fd_stdout == 1);
+
+	FMInit();
+
+	while (1) {
+		//ReadCommand();
+
+		if (currentCmd[0] == 't'&&currentCmd[1] == 'o'&&currentCmd[2] == 'u'&&currentCmd[3] == 'c'&&currentCmd[4] == 'h')
+			Command_touch();
+
+		else if (currentCmd[0] == 'm'&&currentCmd[1] == 'k'&&currentCmd[2] == 'd'&&currentCmd[3] == 'i'&&currentCmd[5] == 'r')
+			Command_mkdir();
+
+		else if (currentCmd[0] == 'r'&&currentCmd[1] == 'm')
+			Command_rm();
+
+		else if (currentCmd[0] == 'r'&&currentCmd[1] == 'm'&&currentCmd[2] == 'd'&&currentCmd[3] == 'i'&&currentCmd[4] == 'r')
+			Command_rmdir();
+
+		else if (currentCmd[0] == 'f'&&currentCmd[1] == 'i'&&currentCmd[2] == 'l'&&currentCmd[3] == 'e')
+			Command_file();
+
+		else if (currentCmd[0] == 'l'&&currentCmd[1] == 's')
+			Command_ls();
+
+		else if (currentCmd[0] == 'c'&&currentCmd[1] == 'd')
+			Command_cd();
+
+		else if (currentCmd[0] == 's'&&currentCmd[1] == 'v')
+			Command_sv();
+
+		else if (!strcmp(cuurentCmd, "help") || !strcmp(cuurentCmd, "Help") || !strcmp(cuurentCmd, "HELP"))
+			ShwoHelp();
+
+		else CommandWrong();
+	}
+}
