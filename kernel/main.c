1863b393ad66d2f1672b5c930fa2ef10daf4f2d8
Chosen_debugger <1023578360@qq.com>
2018-09-07 17:08:54 +0800

    Vol.0.1

---
+++ kernel/main.c
@@ -0,0 +1,116 @@
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                            main.c
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                                    Forrest Yu, 2005
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+//
+//  main.c
+//
+//  Created by wennie on 2017/9/18.
+//  Copyright © 2017年 wennie. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+
+
+
+/*======================================================================*
+                            kernel_main
+ *======================================================================*/
+PUBLIC int kernel_main()
+{
+	disp_str("-----\"kernel_main\" begins-----\n");
+
+	struct task* p_task;
+	struct proc* p_proc= proc_table;
+	char* p_task_stack = task_stack + STACK_SIZE_TOTAL;
+	u16   selector_ldt = SELECTOR_LDT_FIRST;
+        u8    privilege;
+        u8    rpl;
+	int   eflags;
+	int   i, j;
+	int   prio;
+	for (i = 0; i < NR_TASKS+NR_PROCS; i++) {
+	        if (i < NR_TASKS) {     /* 任务 */
+                        p_task    = task_table + i;
+                        privilege = PRIVILEGE_TASK;
+                        rpl       = RPL_TASK;
+                        eflags    = 0x1202; /* IF=1, IOPL=1, bit 2 is always 1 */
+			prio      = 15;
+                }
+                else {                  /* 用户进程 */
+                        p_task    = user_proc_table + (i - NR_TASKS);
+                        privilege = PRIVILEGE_USER;
+                        rpl       = RPL_USER;
+                        eflags    = 0x202; /* IF=1, bit 2 is always 1 */
+			prio      = 5;
+                }
+
+		strcpy(p_proc->name, p_task->name);	/* name of the process */
+		p_proc->pid = i;			/* pid */
+
+		p_proc->ldt_sel = selector_ldt;
+
+		memcpy(&p_proc->ldts[0], &gdt[SELECTOR_KERNEL_CS >> 3],
+		       sizeof(struct descriptor));
+		p_proc->ldts[0].attr1 = DA_C | privilege << 5;
+		memcpy(&p_proc->ldts[1], &gdt[SELECTOR_KERNEL_DS >> 3],
+		       sizeof(struct descriptor));
+		p_proc->ldts[1].attr1 = DA_DRW | privilege << 5;
+		p_proc->regs.cs	= (0 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
+		p_proc->regs.ds	= (8 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
+		p_proc->regs.es	= (8 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
+		p_proc->regs.fs	= (8 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
+		p_proc->regs.ss	= (8 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
+		p_proc->regs.gs	= (SELECTOR_KERNEL_GS & SA_RPL_MASK) | rpl;
+
+		p_proc->regs.eip = (u32)p_task->initial_eip;
+		p_proc->regs.esp = (u32)p_task_stack;
+		p_proc->regs.eflags = eflags;
+
+		/* p_proc->nr_tty		= 0; */
+
+		p_proc->p_flags = 0;
+		p_proc->p_msg = 0;
+		p_proc->p_recvfrom = NO_TASK;
+		p_proc->p_sendto = NO_TASK;
+		p_proc->has_int_msg = 0;
+		p_proc->q_sending = 0;
+		p_proc->next_sending = 0;
+
+		for (j = 0; j < NR_FILES; j++)
+			p_proc->filp[j] = 0;
+
+		p_proc->ticks = p_proc->priority = prio;
+
+		p_task_stack -= p_task->stacksize;
+		p_proc++;
+		p_task++;
+		selector_ldt += 1 << 3;
+	}
+
+	k_reenter = 0;
+	ticks = 0;
+
+	p_proc_ready	= proc_table;
+
+	init_clock();
+        init_keyboard();
+
+	restart();
+
+	while(1){}
+}
+
+/*======================================================================*
+                               TestA
+ *======================================================================*/
+void TestA()
+{
+
+}
